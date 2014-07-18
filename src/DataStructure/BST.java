0c14e5d136d4c197c2585c16aec4bdb494a2be7b
Sunny Nahar <sunnyn@cmu.edu>
2014-07-18 01:25:30 -0400

    Updated BST

--- src/DataStructure/BST.java
+++ src/DataStructure/BST.java
@@ -41,11 +41,11 @@ public class BST {
 	{
 		if(root.right != null)
 			return minimum(root.right);
-		Node p = root.parent;
+		Node p = root.p;
 		while(p != null && p.right == root)
 		{
-			p = p.parent;
-			root = root.parent;
+			p = p.p;
+			root = root.p;
 		}
 		return p;
 	}
@@ -53,29 +53,76 @@ public class BST {
 	{
 		if(root.left != null)
 			return maximum(root.left);
-		Node p = root.parent;
+		Node p = root.p;
 		while(p != null && p.left == root)
 		{
-			p = p.parent;
-			root = root.parent;
+			p = p.p;
+			root = root.p;
 		}
 		return p;
 	}
-	public Node insert(Node root, Node x)
+	public Node insert(Node root, int x)
 	{
-		if(root == null) return x;
-		if(x.value < root.value)
+		Node y = null;
+		while(root != null)
 		{
-			
+			y = root;
+			if(x < root.value)
+				root = root.left;
+			else
+				root = root.right;
 		}
+		Node z = new Node(x);
+		if(y == null)
+			return z;
+
+		z.p = y;
+		if(x < y.value)
+			y.left = z;
+		else
+			y.right = z;
+		return y;
+	}
+	public void delete(Node root, Node x)
+	{
+		if(x == null) return;
+		if(x.left == null)
+			transplant(root,x,x.right);
+		else if(x.right == null)
+			transplant(root,x,x.left);
+		else
+		{
+			Node y = successor(x);
+			if(y.p != x)
+			{
+				transplant(root,y,y.right);
+				y.right = x.right;
+				y.right.p = y;
+			}
+			transplant(root,x,y);
+			y.left = y.left;
+			y.left.p = y;
+		}
+	}
+	public Node transplant(Node root, Node u, Node v)
+	{
+		if(u.p == null)
+			root = v;
+		else if(u == u.p.left)
+			u.p.left = v;
+		else u.p.right = v;
+		if(v != null)
+			v.p = u.p;
+		return root;
 	}
 	private class Node
 	{
-		Node left,right,parent;
+		Node left,right,p;
 		int value;
-		public Node()
+		public Node(int value)
 		{
-			this.parent = null;
+			this.value = value;
+			this.p = null;
 			this.right = null;
 			this.left = null;
 		}	
