e378d6a4a956f92022e673559fb5c63260ef7ad3
Sunny Nahar <sunnyn@cmu.edu>
2014-07-21 23:57:02 -0400

    Added Red-Black tree starter code

---
+++ src/DataStructure/RedBlackTree.java
@@ -0,0 +1,22 @@
+package DataStructure;
+
+public class RedBlackTree {
+	public void left-rotate(Tree t, Node x)
+	{
+		
+	}
+	private class Tree
+	{
+		Node root;
+	}
+	private class Node
+	{
+		boolean color;
+		Node left,right;
+		int value;
+		public Node(int value)
+		{
+			this.value = value;
+		}
+	}
+}
